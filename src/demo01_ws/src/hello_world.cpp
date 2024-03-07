4fdce2bcade5745377f563c22fb2e0f17de2994b
qscesz753 <2847915121@qq.com>
2024-03-07 09:21:39 +0800

    add comments

--- src/demo01_ws/src/hello_world.cpp
+++ src/demo01_ws/src/hello_world.cpp
@@ -1,8 +1,17 @@
+/**
+ * @file hello_world.cpp
+ * @brief
+ * @author Fayin Yu (2847915121@qq.com)
+ * @date 2024-03-07
+ *
+ * @copyright Copyright (C) 2023.
+ *
+ */
 #include "ros/ros.h"
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc,argv,"hello");
+    ros::init(argc,argv,"hello");//ros initialize
     ros::NodeHandle n;
     ROS_INFO("hello world!");
 
